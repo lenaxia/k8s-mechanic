ec624454c1a399bc157d7c1759949dc84d825fb5
Lenaxia <github@47north.lat>
2026-02-21 23:15:12 +0000

    fix(smoke-test): use empty entrypoint override to preserve command execution

--- docker/scripts/smoke-test.sh
+++ docker/scripts/smoke-test.sh
@@ -5,27 +5,8 @@ IMAGE=${1:?Usage: smoke-test.sh <image-tag>}
 
 check() {
     echo "Checking: $*"
-    docker run --rm --entrypoint /bin/sh "$IMAGE" -c "$*"
+    docker run --rm --entrypoint "" "$IMAGE" "$@"
 }
-
-check opencode --version
-check kubectl version --client
-check k8sgpt version
-check helm version
-check flux version --client
-check talosctl version --client
-check kustomize version
-check yq --version
-check gh --version
-check jq --version
-check sops --version
-check age --version
-check stern --version
-check kubeconform -v
-check envsubst --version
-check git --version
-check curl --version
-check openssl version
 
 echo "Checking: agent-entrypoint.sh executable"
 docker run --rm --entrypoint /bin/sh "$IMAGE" -c "test -x /usr/local/bin/agent-entrypoint.sh"
